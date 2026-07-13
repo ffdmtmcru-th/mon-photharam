f6f398ae10cc6a52a783e1bdec4a7028c365bb71
ffdmtmcru-th <ff.dmt.mcru@gmail.com>
2026-07-13 16:29:03 +0700

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Mon Photharam</h1>
+        <p className="text-xl text-gray-600">
+          Integrated Digital Media for Sustainable Tourism
+        </p>
+        <p className="text-lg text-gray-500 mt-4">
+          Case Study: Photharam District, Ratchaburi Province
+        </p>
+      </div>
+    </main>
+  )
+}
